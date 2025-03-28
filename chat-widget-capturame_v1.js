fe7b4f944c1753b433a98e46b28d5f3ed132756e
nextincsolutions <contact@next-inc.solutions>
2025-03-28 10:51:24 -0300

    Update and rename chat-widget-capturame.js to chat-widget-capturame_v1.js

--- chat-widget-capturame_v1.js
+++ chat-widget-capturame_v1.js
@@ -356,6 +356,12 @@
                 </svg>
                 Opções disponíveis
             </button>
+            <button id="openaislicitationchat" class="new-chat-btn" onclick="openChat()">
+                <svg class="message-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M20 2H4c-1.1 0-2 .9-2 2v18l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.2L4 17.2V4h16v12z"/>
+                </svg>
+                Chat licitação
+            </button>
             <p class="response-text">${config.branding.responseTimeText}</p>
         </div>
     `;
